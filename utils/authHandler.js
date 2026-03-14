44d963637fa476a189ff2f34601c5ab95bd4280e
Huynh Tuan <huynhtuan9251@gmail.com>
2026-03-14 15:26:54 +1400

    111

--- utils/authHandler.js
+++ utils/authHandler.js
@@ -1,27 +1,42 @@
-let jwt = require('jsonwebtoken')
-let userController = require("../controllers/users")
+let jwt = require("jsonwebtoken");
+let userController = require("../controllers/users");
+
 module.exports = {
-    checkLogin: async function (req, res, next) {
-        try {
-            let token = req.headers.authorization;
-            if (!token || !token.startsWith('Bearer')) {
-                res.status(404).send("ban chua dang nhap")
-            }
-            token = token.split(" ")[1];
-            let result = jwt.verify(token, "secret");
-            if (result.exp * 1000 > Date.now()) {
-                let user = await userController.FindUserById(result.id);
-                if (user) {
-                    req.user = user
-                    next()
-                } else {
-                    res.status(404).send("ban chua dang nhap")
-                }
-            } else {
-                res.status(404).send("ban chua dang nhap")
-            }
-        } catch (error) {
-            res.status(404).send("ban chua dang nhap")
-        }
+  checkLogin: async function (req, res, next) {
+    try {
+
+      let authHeader = req.headers.authorization;
+
+      // kiểm tra header
+      if (!authHeader || !authHeader.startsWith("Bearer ")) {
+        return res.status(401).send("ban chua dang nhap");
+      }
+
+      // lấy token
+      let token = authHeader.split(" ")[1];
+
+      // verify token
+      let result = jwt.verify(token, "secret");
+
+      // kiểm tra hạn token
+      if (result.exp * 1000 < Date.now()) {
+        return res.status(403).send("token het han");
+      }
+
+      // tìm user
+      let user = await userController.FindUserById(result.id);
+
+      if (!user) {
+        return res.status(401).send("nguoi dung khong ton tai");
+      }
+
+      // lưu user vào request
+      req.user = user;
+
+      next();
+
+    } catch (error) {
+      return res.status(401).send("token khong hop le");
     }
-}
+  }
+};
